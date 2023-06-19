b16cca44220a9ab8d0ec25a55a213917ebb071ed
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-06-19 20:36:22 +0300

    homework #6 ok

--- tests/test_video.py
+++ tests/test_video.py
@@ -1,7 +1,14 @@
-from src.video import PLVideo
+from src.video import Video, PLVideo
 
 
 def test_video():
     vl = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     assert vl.video_url == 'https://youtu.be/4fObz_qw9u4'
     assert vl.__str__() == 'MoscowPython Meetup 78 - вступление'
+
+
+def test_error():
+    vl2 = Video('broken_id')
+    assert vl2.title is None
+    assert vl2.like_count is None
+    assert vl2.view_count is None
